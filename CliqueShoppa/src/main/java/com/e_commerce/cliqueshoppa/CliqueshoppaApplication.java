bf27b35314ec88ac35893e4e59308cf47a988bb4
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-07-21 21:29:44 +0100

    project setup

---
+++ CliqueShoppa/src/main/java/com/e_commerce/cliqueshoppa/CliqueshoppaApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.cliqueshoppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliqueshoppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliqueshoppaApplication.class, args);
+	}
+
+}
